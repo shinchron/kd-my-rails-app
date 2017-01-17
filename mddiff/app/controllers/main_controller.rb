f12731b7cb2b3d22f984b069186ce5ff4e9732ed
(shinchron) <(suzu12shin28@i.softbank.jp)>
2017-01-17 03:06:09 +0000

    1/17授業終了　(Level 2-2)

--- mddiff/app/controllers/main_controller.rb
+++ mddiff/app/controllers/main_controller.rb
@@ -1,7 +1,27 @@
+require "open3"
+
+def exec_pandoc(input, from)
+  pandoc_command = "pandoc -f #{from} -t html"
+  logger.debug 'pandoc_command: ' + pandoc_command
+  output, status = Open3.capture2(pandoc_command, :stdin_data=>input)
+  output
+end
+
 class MainController < ApplicationController
   def index
+    @input_md = ''
+    @output_pandoc = ''
+    @output_github = ''
+    @output_multi = ''
   end
 
   def convert
+    @input_md = params[:'input-markdown']
+
+    @output_pandoc = exec_pandoc(@input_md, 'markdown')
+    @output_github = exec_pandoc(@input_md, 'markdown_github')
+    @output_multi  = exec_pandoc(@input_md, 'markdown_mmd')
+
+    render action: :index
   end
 end
